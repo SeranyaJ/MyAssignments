b434b5617c7804fe78a85648725cc6d060c9fd9e
SeranyaJ <jakkireddy.seranya@gmail.com>
2025-09-13 16:32:45 +0530

    Add files via upload
    Day1 assessment

---
+++ day1/Chrome.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Chrome {
+ public static void main(String args[]) {
+	 float version = 90.1f;
+	 String developer="Google";
+	 boolean byteOftype = false;
+	 int releaseOfyear =2008;
+	 char shortcutKey='C';
+	 System.out.println(version+" " + developer+" " +byteOftype+" " +releaseOfyear+" " +shortcutKey);
+	 
+ }
+}
